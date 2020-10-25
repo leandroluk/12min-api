c75ec2c9cd9fb6fef42ea522e1b0f4fc6a4118b0
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-10-25 18:39:38 -0300

    feat: create mongo helper and add user repo

---
+++ src/infra/db/mongodb/repos/add-user.repository.ts
@@ -0,0 +1,22 @@
+import { IAddUserRepository } from '@/data/protocols/add-user.repository'
+import { IEncrypter } from '@/data/protocols/encrypter'
+import { IUserModel } from '@/domain/models/user.model'
+import { IAddUserModel } from '@/domain/use-cases/add-user'
+import { MongoHelper } from '../helpers/mongo.helper'
+
+export class MongoAddUserRepository implements IAddUserRepository {
+  constructor(
+    readonly encrypter: IEncrypter
+  ) { }
+
+  async addUser(userData: IAddUserModel): Promise<IUserModel> {
+    const userCollection = MongoHelper.getCollection('users')
+    const data = {
+      ...userData,
+      createdAt: new Date(),
+      secret: await this.encrypter.encrypt(userData.email + userData.password)
+    }
+    const result = await userCollection.insertOne(data)
+    return MongoHelper.map<IUserModel>(result.ops[0])
+  }
+}
