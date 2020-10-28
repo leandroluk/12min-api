30e7ecf161825a678013e19183dc29a2304b1f7b
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-10-28 16:30:32 -0300

    feat: add enum Status for Audiobook Model

--- src/domain/models/audiobook.model.ts
+++ src/domain/models/audiobook.model.ts
@@ -1,8 +1,12 @@
+export enum AudiobookStatus {
+  PENDING = 'pending',
+}
+
 export interface IAudiobookModel {
   id: string
-  status: string
+  status: AudiobookStatus
   createdAt: Date
-  updatedAt: Date
+  updatedAt?: Date
   title: string
   description: string
   filePath: string
