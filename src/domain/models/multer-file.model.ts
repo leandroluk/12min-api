11ec9548f16df3a66bd004975a46e77813a49bd5
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-10-28 15:19:25 -0300

    feat: add model to capture multer received file

---
+++ src/domain/models/multer-file.model.ts
@@ -0,0 +1,10 @@
+export interface IMulterFile {
+  destination: string
+  encoding: string
+  fieldname: string
+  filename: string
+  mimetype: string
+  originalname: string
+  path: string
+  size: number
+}
