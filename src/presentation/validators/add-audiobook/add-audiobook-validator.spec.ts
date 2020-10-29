d1f6624ded81b7e4bbd2f52bb4635b70ed2f5843
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-10-29 14:11:27 -0300

    feat: create validator to audiobook

--- src/presentation/validators/add-audiobook/add-audiobook-validator.spec.ts
+++ src/presentation/validators/add-audiobook/add-audiobook-validator.spec.ts
@@ -1,6 +1,8 @@
 import faker from 'faker'
 import { IAddAudiobookModel } from '../../../domain/use-cases/add-audiobook'
 import { IAddAudiobookValidate } from '../../../domain/use-cases/add-audiobook-validate'
+import { IFileExistsValidator } from '../../protocols/file-exists-validator'
+import { IFileExtensionValidator } from '../../protocols/file-extension-validator'
 import { INullValidator } from '../../protocols/null-validator'
 import { AddAudiobookValidator } from './add-audiobook-validator'
 
@@ -13,43 +15,90 @@ const makeNullValidator = (): INullValidator => {
   return new NullValidatorStub()
 }
 
+const makeFileExtensionValidator = (): IFileExtensionValidator => {
+  class FileExtensionValidatorStub implements IFileExtensionValidator {
+    async isFileExtension(_filePath: any): Promise<boolean> {
+      return await Promise.resolve(true)
+    }
+  }
+  return new FileExtensionValidatorStub()
+}
+
+const makeFileExistsValidator = (): IFileExistsValidator => {
+  class FileExistsValidatorStub implements IFileExistsValidator {
+    async fileExists(_path: any): Promise<boolean> {
+      return await Promise.resolve(true)
+    }
+  }
+  return new FileExistsValidatorStub()
+}
+
 const makeSut = (): {
   addAudiobookModel: IAddAudiobookModel
   nullValidator: INullValidator
+  fileExtensionValidator: IFileExtensionValidator
+  fileExistsValidator: IFileExistsValidator
   sut: IAddAudiobookValidate
 } => {
   const addAudiobookModel = {
     title: faker.name.title(),
     description: faker.random.words(4),
+    filePath: 'file/path.mp3',
     tags: [faker.random.word()]
   }
   const nullValidator = makeNullValidator()
-  const sut = new AddAudiobookValidator(nullValidator)
+  const fileExtensionValidator = makeFileExtensionValidator()
+  const fileExistsValidator = makeFileExistsValidator()
+  const sut = new AddAudiobookValidator(nullValidator, fileExtensionValidator, fileExistsValidator)
 
   return {
     addAudiobookModel,
     nullValidator,
+    fileExtensionValidator,
+    fileExistsValidator,
     sut
   }
 }
 
 describe('AddAudiobookValidator', () => {
   describe('validateAddAudiobook', () => {
+    test('should all validators to be called', async () => {
+      const { sut, nullValidator, fileExtensionValidator, fileExistsValidator, addAudiobookModel } = makeSut()
+      const isNullSpy = jest.spyOn(nullValidator, 'isNull')
+      const isFileExtensionSpy = jest.spyOn(fileExtensionValidator, 'isFileExtension')
+      const fileExistsSpy = jest.spyOn(fileExistsValidator, 'fileExists')
+      await sut.validateAddAudiobook(addAudiobookModel)
+      expect(isNullSpy).toHaveBeenCalled()
+      expect(isFileExtensionSpy).toHaveBeenCalledWith(addAudiobookModel.filePath)
+      expect(fileExistsSpy).toHaveBeenCalledWith(addAudiobookModel.filePath)
+    })
+
     test('should return a object with missing param error if some required field isn\t provided', async () => {
       const { sut, nullValidator, addAudiobookModel } = makeSut()
       jest.spyOn(nullValidator, 'isNull').mockResolvedValue(true)
       const result = await sut.validateAddAudiobook(addAudiobookModel)
       expect(result.title.message).toMatch(/Missing param.*title.*?/)
       expect(result.description.message).toMatch(/Missing param.*description.*?/)
+      expect(result.filePath.message).toMatch(/Missing param.*filePath.*?/)
       expect(result.tags.message).toMatch(/Missing param.*tags.*?/)
     })
 
     test('should return a object with invalid param error if some field is invalid', async () => {
-      const { sut } = makeSut()
-      const result = await sut.validateAddAudiobook({ title: 1, description: 1, tags: 1 } as any)
+      const { sut, fileExtensionValidator, fileExistsValidator } = makeSut()
+      let result
+
+      result = await sut.validateAddAudiobook({ title: 1, description: 1, filePath: 1, tags: 1 } as any)
       expect(result.title.message).toMatch(/Invalid param.*title.*?/)
       expect(result.description.message).toMatch(/Invalid param.*description.*?/)
       expect(result.tags.message).toMatch(/Invalid param.*tags.*?/)
+
+      jest.spyOn(fileExtensionValidator, 'isFileExtension').mockResolvedValueOnce(false)
+      result = await sut.validateAddAudiobook({ title: 1, description: 1, filePath: 1, tags: 1 } as any)
+      expect(result.filePath.message).toMatch(/Invalid param.*filePath.*?/)
+
+      jest.spyOn(fileExistsValidator, 'fileExists').mockResolvedValueOnce(false)
+      result = await sut.validateAddAudiobook({ title: 1, description: 1, filePath: 1, tags: 1 } as any)
+      expect(result.filePath.message).toMatch(/Invalid param.*filePath.*?/)
     })
 
     test('should return a object with invalid param error if some value inner tags is invalid', async () => {
--- src/presentation/validators/add-audiobook/add-audiobook-validator.ts
+++ src/presentation/validators/add-audiobook/add-audiobook-validator.ts
@@ -2,11 +2,15 @@ import { IAddAudiobookModel } from '../../../domain/use-cases/add-audiobook'
 import { IAddAudiobookValidate } from '../../../domain/use-cases/add-audiobook-validate'
 import { InvalidParamError } from '../../../errors/invalid-param/invalid-param.error'
 import { MissingParamError } from '../../../errors/missing-param/missing-param.error'
+import { IFileExistsValidator } from '../../protocols/file-exists-validator'
+import { IFileExtensionValidator } from '../../protocols/file-extension-validator'
 import { INullValidator } from '../../protocols/null-validator'
 
 export class AddAudiobookValidator implements IAddAudiobookValidate {
   constructor(
-    readonly nullValidator: INullValidator
+    readonly nullValidator: INullValidator,
+    readonly fileExtensionValidator: IFileExtensionValidator,
+    readonly fileExistsValidator: IFileExistsValidator
   ) { }
 
   async validateAddAudiobook(addAudiobook: IAddAudiobookModel): Promise<any> {
@@ -16,7 +20,7 @@ export class AddAudiobookValidator implements IAddAudiobookValidate {
       errors.title = new MissingParamError('title')
     } else {
       if (typeof addAudiobook.title !== 'string') {
-        errors.title = new InvalidParamError('title', 'must be a string')
+        errors.title = new InvalidParamError('title', 'must be a string.')
       }
     }
 
@@ -24,7 +28,17 @@ export class AddAudiobookValidator implements IAddAudiobookValidate {
       errors.description = new MissingParamError('description')
     } else {
       if (typeof addAudiobook.description !== 'string') {
-        errors.description = new InvalidParamError('description', 'must be a string')
+        errors.description = new InvalidParamError('description', 'must be a string.')
+      }
+    }
+
+    if (await this.nullValidator.isNull(addAudiobook.filePath)) {
+      errors.filePath = new MissingParamError('filePath')
+    } else {
+      if (!await this.fileExtensionValidator.isFileExtension(addAudiobook.filePath)) {
+        errors.filePath = new InvalidParamError('filePath', 'must be a MP3 or WAV.')
+      } else if (!await this.fileExistsValidator.fileExists(addAudiobook.filePath)) {
+        errors.filePath = new InvalidParamError('filePath', 'could not find the file')
       }
     }
 
