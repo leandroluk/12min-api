3cf533384d460c560372035f29c056b36135dfab
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-10-30 11:46:40 -0300

    feat: create implementation of db add audiobook

---
+++ src/data/use-cases/db-add-audiobook.spec.ts
@@ -0,0 +1,68 @@
+import { AudiobookStatus, IAudiobookModel } from '../../domain/models/audiobook.model'
+import { IAddAudiobookModel } from '../../domain/use-cases/add-audiobook'
+import { IAddAudiobookRepository } from '../protocols/add-audiobook.repository'
+import { DbAddAudiobook } from './db-add-audiobook'
+
+const makeAddAudiobookRepository = (): IAddAudiobookRepository => {
+  class AddAudiobookRepositoryStub implements IAddAudiobookRepository {
+    async addAudiobook(audiobook: IAddAudiobookModel): Promise<IAudiobookModel> {
+      return await Promise.resolve({
+        id: 'id',
+        createdAt: new Date(),
+        title: audiobook.title,
+        description: audiobook.description,
+        tags: audiobook.tags,
+        status: AudiobookStatus.PENDING
+      })
+    }
+  }
+  return new AddAudiobookRepositoryStub()
+}
+
+const makeSut = (): {
+  addAudiobookRepository: IAddAudiobookRepository
+  sut: DbAddAudiobook
+  addAudiobookModel: IAddAudiobookModel
+} => {
+  const addAudiobookRepository = makeAddAudiobookRepository()
+  const sut = new DbAddAudiobook(addAudiobookRepository)
+  const addAudiobookModel = {
+    title: 'title',
+    description: 'description',
+    tags: ['tags']
+  }
+
+  return {
+    addAudiobookRepository,
+    sut,
+    addAudiobookModel
+  }
+}
+
+describe('DbAddAudiobook', () => {
+  describe('addAudiobook', () => {
+    test('should call IAddAudiobookRepository', async () => {
+      const { sut, addAudiobookRepository, addAudiobookModel } = makeSut()
+      const addAudiobookSpy = jest.spyOn(addAudiobookRepository, 'addAudiobook')
+      await sut.addAudiobook(addAudiobookModel)
+      expect(addAudiobookSpy).toHaveBeenCalled()
+    })
+
+    test('should throw if IAddAudiobookRepository throws', () => {
+      const { sut, addAudiobookRepository } = makeSut()
+      jest.spyOn(addAudiobookRepository, 'addAudiobook').mockRejectedValue(new Error())
+      expect(sut.addAudiobook({} as any)).rejects.toThrow()
+    })
+
+    test('should return AddAudiobookReturn if audiobook is created', async () => {
+      const { sut, addAudiobookModel } = makeSut()
+      const result = await sut.addAudiobook(addAudiobookModel)
+      expect(result.id).toBe('id')
+      expect(result.createdAt.constructor.name).toBe('Date')
+      expect(result.title).toBe(addAudiobookModel.title)
+      expect(result.description).toBe(addAudiobookModel.description)
+      expect(result.tags[0]).toBe('tags')
+      expect(result.status).toBe('pending')
+    })
+  })
+})
---
+++ src/data/use-cases/db-add-audiobook.ts
@@ -0,0 +1,19 @@
+import { AudiobookStatus } from '../../domain/models/audiobook.model'
+import { IAddAudiobook, IAddAudiobookModel, IAddAudiobookReturn } from '../../domain/use-cases/add-audiobook'
+import { IAddAudiobookRepository } from '../protocols/add-audiobook.repository'
+
+export class DbAddAudiobook implements IAddAudiobook {
+  constructor(
+    readonly addAudiobookRepository: IAddAudiobookRepository
+  ) { }
+
+  async addAudiobook(audiobookData: IAddAudiobookModel): Promise<IAddAudiobookReturn> {
+    const data = { ...audiobookData }
+
+    const audiobook = await this.addAudiobookRepository.addAudiobook(data)
+    return {
+      ...audiobook,
+      status: AudiobookStatus.PENDING
+    }
+  }
+}
