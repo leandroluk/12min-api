3f3071252e19704b19ede477b3f6550558e48581
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-10-31 16:37:43 -0300

    refactor: improve name of validator

--- src/presentation/validators/convert-file/convert-file.validator.ts
+++ src/presentation/validators/convert-file/convert-file.validator.ts
@@ -1,18 +1,19 @@
-import { IConvertAudioFileValidate } from '../../../domain/use-cases/convert-audio-file-validate'
+import { IConvertFileValidate } from '../../../domain/use-cases/convert-file-validate'
 import { InvalidParamError } from '../../../errors/invalid-param/invalid-param.error'
 import { MissingParamError } from '../../../errors/missing-param/missing-param.error'
+import env from '../../../main/config/env'
 import { IFileExistsValidator } from '../../protocols/file-exists-validator'
 import { IFileExtensionValidator } from '../../protocols/file-extension-validator'
 import { INullValidator } from '../../protocols/null-validator'
 
-export class ConvertAudioFileValidator implements IConvertAudioFileValidate {
+export class ConvertAudioFileValidator implements IConvertFileValidate {
   constructor(
     readonly nullValidator: INullValidator,
     readonly fileExtensionValidator: IFileExtensionValidator,
     readonly fileExistsValidator: IFileExistsValidator
   ) { }
 
-  async validateConvertAudioFile(convertAudioFile: string): Promise<Error> {
+  async validateConvertFile(convertAudioFile: string): Promise<Error> {
     if (await this.nullValidator.isNull(convertAudioFile)) {
       return new MissingParamError('convertAudioFile')
     }
@@ -20,7 +21,7 @@ export class ConvertAudioFileValidator implements IConvertAudioFileValidate {
       return new InvalidParamError('convertAudioFile', 'must be a string')
     }
     if (!await this.fileExtensionValidator.isFileExtension(convertAudioFile)) {
-      return new InvalidParamError('convertAudioFile', 'must be a MP3 or WAV file')
+      return new InvalidParamError('convertAudioFile', `must be one of ${env.converters.fileExtensionMatchers.join()} file`)
     }
     if (!await this.fileExistsValidator.fileExists(convertAudioFile)) {
       return new InvalidParamError('convertAudioFile', 'file no exists')
