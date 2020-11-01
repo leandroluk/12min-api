6477d5195f106b4ad16ceb1e9f3f4da4dd20f67f
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-11-01 00:14:43 -0300

    feat: create interface of getAudiobook params

--- src/domain/use-cases/get-audiobook.ts
+++ src/domain/use-cases/get-audiobook.ts
@@ -1,5 +1,9 @@
 import { IAudiobookModel } from '../models/audiobook.model'
 
+export interface IGetAudiobookParams {
+  audiobookId: string
+}
+
 export interface IGetAudiobook {
   getAudiobook(audiobookId: string): Promise<IAudiobookModel>
 }
