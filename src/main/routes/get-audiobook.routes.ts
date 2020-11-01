ceb0e449afa64348688b6b2b9307f9334673a2dd
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-11-01 03:07:40 -0300

    feat: finish get audiobook route

---
+++ src/main/routes/get-audiobook.routes.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import jsonRouteAdapter from '../adapters/json-route.adapter'
+import env from '../config/env'
+import { makeGetAudiobookController } from '../factories/get-audiobook.factory'
+
+export default (router: Router): void => {
+  router.get(env.routes.getAudiobook, jsonRouteAdapter(makeGetAudiobookController())
+  )
+}
