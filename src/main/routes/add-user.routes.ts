45dfa9aa080cdf5fa1b7c3fd7f2a93126452c094
Leandro Santiago Gomes <leandroluk@gmail.com>
2020-11-01 16:47:02 -0300

    refactor: indent route declaration

--- src/main/routes/add-user.routes.ts
+++ src/main/routes/add-user.routes.ts
@@ -4,5 +4,8 @@ import env from '../config/env'
 import { makeAddUserController } from '../factories/add-user.factory'
 
 export default (router: Router): void => {
-  router.post(env.routes.addUser, jsonRouteAdapter(makeAddUserController()))
+  router.post(
+    env.routes.addUser,
+    jsonRouteAdapter(makeAddUserController())
+  )
 }
